fe53992421476ccf97ead923ed0db01268e0eb18
yangjie <yangj@myibc.net>
2020-04-08 14:49:15 +0800

    1. Code optimization 2. Add customized configuration files

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 ```shell
 npm i uix-precompile-loader -D
 ```
+
 ```javascript
 //webpack config
 module.exports = {
@@ -14,7 +15,8 @@ module.exports = {
                 loader: "uix-precompile-loader",
                 options:{
                     /** process.env[`platformName`] */
-                    platformName:"platform"
+                    platformName:"platform",
+                    configPath:path.join(__dirname,'./config.json')
                 }
             }
         ]
@@ -27,4 +29,84 @@ console.log('开发环境输出');
 //#IF PROD
 console.log('生产环境输出');
 //#IF
+
+/** HTML Example **/
+<!-- #IF DEV -->
+<div>开发环境输出</div>
+<!-- #IF -->
+<!-- #IF PROD -->
+<div>生产环境输出</div>
+<!-- #IF -->
 ```
+
+
+### configPath 应用定制化编译
+```javascript
+//config.json
+{
+    "module":{
+        "des":"模块1",
+        "enabled":true,
+        "children":{
+            "module-child1":{
+                "enabled":true,
+                "name":"模块1-子模块1"
+            },
+            "module-child2":{
+                "enabled":true,
+                "name":"模块1-子模块2"
+            },
+            "module-child3":{
+                "enabled":true,
+                "name":"模块1-子模块3"
+            }
+        }
+    },
+    "module1":{
+        "des":"模块2",
+        "enabled":true,
+        "children":{
+            "module1-child1":{
+                "enabled":true,
+                "name":"模块2-子模块1"
+            },
+            "module1-child2":{
+                "enabled":true,
+                "name":"模块2-子模块2"
+            },
+            "module1-child3":{
+                "enabled":true,
+                "name":"模块2-子模块3"
+            }
+        }
+    },
+    "module3":{
+        "des":"模块3",
+        "enabled":true,
+        "children":{
+            "module2-child1":{
+                "enabled":true,
+                "name":"模块3-子模块1"
+            },
+            "module2-child2":{
+                "enabled":true,
+                "name":"模块3-子模块2"
+            },
+            "module2-child3":{
+                "enabled":true,
+                "name":"模块3-子模块3"
+            }
+        }
+    }
+};
+/** 配置文件的Key是条件判断 **/
+//#IF module
+console.log('模块1');
+//#IF
+```
+
+## 注意
+```
+nodejs环境变量（process.env）要在启动webpack前注入不然无效
+```
+
--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 var utils = require('loader-utils');
+var fs = require('fs');
 var getOptions = utils.getOptions;
-module.exports = function(source) {
-    var options = getOptions(this);
-    options = options || new Object();
+var otherPrecompile = function(source,condition){
     var reg = /(\/\/\#[i|I][f|F])((?!\n\r)\s)(((?!(#[i|I][f|F])).)+)[\r\n](((?!(#[i|I][f|F]))[\s\S])+)[\r\n]((?![\r|\n])\s)*(\/\/\#[i|I][f|F])/g;
-    var htmlReg = /(\<\!\-\-\#[i|I][f|F])((?!\n\r)\s)(((?!(#[i|I][f|F])).)+)[\r\n](((?!(#[i|I][f|F]))[\s\S])+)[\r\n]((?![\r|\n])\s)*(\#[i|I][f|F]\-\-\>)/g;
-    var name = options.platformName || 'platform';
-    var platform = process.env[name] || '';
-    var platformDatas = platform.split(',');
-    source = source.replace(reg, value => {
+    condition = condition || new Array();
+    if(!source) return source;
+    source = source.replace(reg,value=>{
         var regs = /(\/\/\#[i|I][f|F])((?!\n\r)\s)(((?!(#[i|I][f|F])).)+)[\r\n]/g;
         let platformValue = "";
         value.replace(regs, env => {
             platformValue = env.replace(/\/\/\#[i|I][f|F]/g, '').trim();
             return env;
         });
-        if (platformDatas.indexOf(platformValue) >= 0) {
+        if (condition.indexOf(platformValue) >= 0) {
             var regsL = /(\/\/\#[i|I][f|F])((?!\n\r)\s)(((?!(#[i|I][f|F])).)+)[\r\n]/g;
             var regsR = /[\r\n]((?![\r|\n])\s)*(\/\/\#[i|I][f|F])[\r\n]{0,1}/g;
             value = value.replace(regsL, '').replace(regsR, '');
@@ -23,20 +20,67 @@ module.exports = function(source) {
         }
         return '';
     });
-    source = source.replace(htmlReg, value => {
-        var regs = /(\<\!\-\-\#[i|I][f|F])((?!\n\r)\s)(((?!(#[i|I][f|F])).)+)[\r\n]/g;
+    return source;
+    
+};
+var htmlPrecompile = function(source,condition){
+    var reg = /(\<\!\-\-(\s*)\#[i|I][f|F])(((?!(#[i|I][f|F]))[\s\S])+)(((?!(#[i|I][f|F|]))[\s\S])+)(\#[i|I][f|F](\s*)\-\-\>)/g;
+    condition = condition || new Array();
+    if(!source) return source;
+    source = source.replace(reg,value=>{
+        var regs = /(\<\!\-\-(\s*)\#[i|I][f|F])((?!\n\r)\s)(((?!(#[i|I][f|F])).)+)(\s*)(\-\-\>)[\r\n]/g;
         let platformValue = "";
         value.replace(regs, env => {
-            platformValue = env.replace(/\<\!\-\-\#[i|I][f|F]/g, '').trim();
+            platformValue = env.replace(/\<\!\-\-/g,'').replace(/\-\-\>/g,'').replace(/\#IF/g,'').trim();
             return env;
         });
-        if (platformDatas.indexOf(platformValue) >= 0) {
-            var regsL = /(\<\!\-\-\#[i|I][f|F])((?!\n\r)\s)(((?!(#[i|I][f|F])).)+)[\r\n]/g;
-            var regsR = /[\r\n]((?![\r|\n])\s)*(\#[i|I][f|F]\-\-\>)[\r\n]{0,1}/g;
-            value = value.replace(regsL, '').replace(regsR, '');
+        if (condition.indexOf(platformValue) >= 0) {
+            value = value.replace(regs, '').replace(/\<\!\-\-/g,'').replace(/\-\-\>/g,'').replace(/\#IF/g,'').trim();
             return value;
         }
         return '';
     });
+    return source;
+};
+var recursionGetCondition = (data)=>{
+    data = data || new Object();
+    var result = new Array();
+    const keys = Object.keys(data);
+    keys.map(key=>{
+        const val = data[key];
+        var enabled = false;
+        if(typeof val === 'boolean') enabled = val;
+        if(typeof val === 'object' && !(val instanceof Array)){
+            enabled = Boolean(val.enabled);
+        }
+        if(enabled) result.push(key);
+        if(!enabled) return;
+        if(typeof val === 'object' && !(val instanceof Array) && val.children){
+            var childResult = recursionGetCondition(val.children);
+            result = result.concat(childResult);
+        }
+    });
+    return result;
+}
+module.exports = function(source) {
+    var options = getOptions(this);
+    options = options || new Object();
+    let configPath = options.configPath;
+    var condition = new Array();
+    if(fs.existsSync(configPath)){
+        try{
+            var conditionStr = fs.readFileSync(configPath,'utf-8');
+            var configPathJson = JSON.parse(conditionStr);
+            condition = recursionGetCondition(configPathJson);
+        }catch(e){
+            console.error('The configuration file sequence failed!');
+        }
+    }else{
+        var name = options.platformName || 'platform';
+        var platform = process.env[name] || '';
+        condition = platform.split(',');
+    };
+    source = otherPrecompile(source,condition);
+    source = htmlPrecompile(source,condition);
     return source;
 }
